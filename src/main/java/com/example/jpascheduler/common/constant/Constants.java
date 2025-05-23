269cb7692c6dc9da325662a60ab4af7625c496e2
taeung515 <xodndd515@gmail.com>
2025-05-23 10:23:05 +0900

    chore: 세션 저장할 상수 생성

---
+++ src/main/java/com/example/jpascheduler/common/constant/Constants.java
@@ -0,0 +1,11 @@
+package com.example.jpascheduler.common.constant;
+
+public final class Constants {
+
+    private Constants() {
+
+    }
+
+    public static final String SESSION_USER_EMAIL = "loginUserEmail";
+
+}
