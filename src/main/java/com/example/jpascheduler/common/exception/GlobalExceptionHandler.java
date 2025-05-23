b56b7f7c34b91d87ff05feebc9ef0cac4fe7e0d2
taeung515 <xodndd515@gmail.com>
2025-05-23 17:47:16 +0900

    feat: 검증실패 시 defalut 메시지를 사용자에게 응답하도록 전역예외처리 추가
    - MethodArgumentNotValidException 핸들러에서 FieldError.getDefaultMessage() 반환

---
+++ src/main/java/com/example/jpascheduler/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.example.jpascheduler.common.exception;
+
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public String processValidationError(MethodArgumentNotValidException exception) {
+        BindingResult bindingResult = exception.getBindingResult();
+
+        StringBuilder builder = new StringBuilder();
+        for (FieldError fieldError : bindingResult.getFieldErrors()) {
+            builder.append(fieldError.getDefaultMessage());
+            builder.append(" 입력된 값: ");
+            builder.append(fieldError.getRejectedValue());
+            builder.append(" \n");
+        }
+
+        return builder.toString();
+    }
+}
+
+
+
+
+
