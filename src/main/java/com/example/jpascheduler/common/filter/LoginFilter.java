388ca4eeaaedf9b1161e39a88f93b46c2691cedc
taeung515 <xodndd515@gmail.com>
2025-05-23 10:26:11 +0900

    chore: 로그인 인증 필터 추가
    -/api/users/signup, /login 제외하고 세션없이 접근 시 접근 차단

---
+++ src/main/java/com/example/jpascheduler/common/filter/LoginFilter.java
@@ -0,0 +1,41 @@
+package com.example.jpascheduler.common.filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.util.PatternMatchUtils;
+import java.io.IOException;
+
+import static com.example.jpascheduler.common.constant.Constants.*;
+
+
+
+public class LoginFilter implements Filter {
+
+    private static final String[] WHITE_LIST = {"/api/users/signup", "/api/users/login"};
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        String requestURI = httpRequest.getRequestURI();
+
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+
+        if (!isWhiteList(requestURI)) {
+
+            HttpSession session = httpRequest.getSession(false);
+
+            if (session == null || session.getAttribute(SESSION_USER_EMAIL) == null) {
+                throw new RuntimeException("로그인 해주세요");
+            }
+
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private boolean isWhiteList(String requestURI) {
+        return PatternMatchUtils.simpleMatch(WHITE_LIST, requestURI);
+    }
+}
